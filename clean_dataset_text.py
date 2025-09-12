c8e317f72bc049c418d67c15079aa0fbf859db57
James Allen-Robertson <minyall@gmail.com>
2025-09-12 17:30:14 +0100

    Implement but not use stop word list option to clean dataset script

--- clean_dataset_text.py
+++ clean_dataset_text.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import spacy
 from pathlib import Path
+import urllib.request as ur
 
 def clean_guardian_text(text:str, remove_elements:tuple[str]=('span','aside')) ->str:
     soup = BeautifulSoup(text, 'html.parser')
@@ -10,12 +11,14 @@ def clean_guardian_text(text:str, remove_elements:tuple[str]=('span','aside')) -
     cleaned_item ='\n'.join(paras)
     return cleaned_item
 
-def tokenise_doc(doc:spacy.tokens.Doc) -> str:
+def tokenise_doc(doc:spacy.tokens.Doc, stop_list:list[str] = None) -> str:
     tokens = [w.lemma_.lower() for w in doc if not w.is_stop and w.is_alpha]
+    if stop_list:
+        tokens = [t for t in tokens if t not in stop_list]
     return ' '.join(tokens)
 
 if __name__ == '__main__':
-    PATH_STR = "4_topic_modelling/farage_large_dataset.parquet"
+    PATH_STR = "2_text_prep/farright_dataset.parquet"
     FILE_PATH = Path(PATH_STR)
     DESTINATION_PATH = FILE_PATH.with_stem(FILE_PATH.stem + '_cleaned')
 
@@ -27,6 +30,10 @@ if __name__ == '__main__':
 
     BATCH_SIZE = 150
     WORKERS = -1
+
+#     stop_list = ur.urlopen(
+#     "https://github.com/first20hours/google-10000-english/raw/refs/heads/master/google-10000-english.txt"
+# ).read().decode('utf-8').split('\n')
 
     tokens = []
     for doc in nlp.pipe(df['cleaned_text'].tolist(), batch_size=BATCH_SIZE, n_process=WORKERS):
